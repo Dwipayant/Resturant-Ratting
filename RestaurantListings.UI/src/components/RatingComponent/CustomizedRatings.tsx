0260601033ca8ddbd95552722b825f57c0899761
Jajati <jajatibadu@gmail.com>
2021-07-16 21:29:23 +0530

    ratting added to the app

---
+++ RestaurantListings.UI/src/components/RatingComponent/CustomizedRatings.tsx
@@ -0,0 +1,62 @@
+import React from 'react';
+import { withStyles } from '@material-ui/core/styles';
+import Rating from '@material-ui/lab/Rating';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import SentimentVeryDissatisfiedIcon from '@material-ui/icons/SentimentVeryDissatisfied';
+import SentimentDissatisfiedIcon from '@material-ui/icons/SentimentDissatisfied';
+import SentimentSatisfiedIcon from '@material-ui/icons/SentimentSatisfied';
+import SentimentSatisfiedAltIcon from '@material-ui/icons/SentimentSatisfiedAltOutlined';
+import SentimentVerySatisfiedIcon from '@material-ui/icons/SentimentVerySatisfied';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
+
+const StyledRating = withStyles({
+    iconFilled: {
+        color: '#ff6d75',
+    },
+    iconHover: {
+        color: '#ff3d47',
+    },
+})(Rating);
+
+const customIcons = {
+    1: {
+        icon: <SentimentVeryDissatisfiedIcon />,
+        label: 'Very Dissatisfied',
+    },
+    2: {
+        icon: <SentimentDissatisfiedIcon />,
+        label: 'Dissatisfied',
+    },
+    3: {
+        icon: <SentimentSatisfiedIcon />,
+        label: 'Neutral',
+    },
+    4: {
+        icon: <SentimentSatisfiedAltIcon />,
+        label: 'Satisfied',
+    },
+    5: {
+        icon: <SentimentVerySatisfiedIcon />,
+        label: 'Very Satisfied',
+    },
+};
+
+export default function CustomizedRatings(props: any) {
+    console.log(props);
+    const { restaurantName } = props;
+    return (
+        <div>
+            <Box component="fieldset" mb={3} borderColor="transparent">
+                <Typography component="legend">Custom icon and color</Typography>
+                <StyledRating
+                    name={restaurantName}
+                    defaultValue={2}
+                    getLabelText={(value) => `${value} Heart${value !== 1 ? 's' : ''}`}
+                    precision={0.5}
+                    icon={<FavoriteIcon fontSize="inherit" />}
+                />
+            </Box>
+        </div>
+    );
+}

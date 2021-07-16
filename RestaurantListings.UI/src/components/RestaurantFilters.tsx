690f67d9853b56f6b8ddbff824cc5eeeebc2ff3d
Jajati <jajatibadu@gmail.com>
2021-07-16 22:26:56 +0530

    Material responsive

--- RestaurantListings.UI/src/components/RestaurantFilters.tsx
+++ RestaurantListings.UI/src/components/RestaurantFilters.tsx
@@ -2,103 +2,143 @@ import React, { useReducer, Reducer, useEffect } from "react";
 import styled from "@emotion/styled";
 
 import { ToggleFilter } from "./ToggleFilter";
+import { AppBar, Container, CssBaseline, makeStyles } from "@material-ui/core";
 
 export interface RestaurantFiltersState {
-  tags: string[];
-  isFamilyFriendly: boolean;
-  isVeganFriendly: boolean;
+    tags: string[];
+    isFamilyFriendly: boolean;
+    isVeganFriendly: boolean;
 }
 
 type RestaurantFiltersAction =
-  | { type: "toggleTag"; payload: { tag: string } }
-  | { type: "toggleFamilyFriendly" }
-  | { type: "toggleVeganFriendly" };
+    | { type: "toggleTag"; payload: { tag: string } }
+    | { type: "toggleFamilyFriendly" }
+    | { type: "toggleVeganFriendly" };
 
 const restaurantFiltersReducer: Reducer<
-  RestaurantFiltersState,
-  RestaurantFiltersAction
+    RestaurantFiltersState,
+    RestaurantFiltersAction
 > = (state, action) => {
-  switch (action.type) {
-    case "toggleTag": {
-      const tagIndex = state.tags.indexOf(action.payload.tag);
-      if (tagIndex !== -1) {
-        return { ...state, tags: state.tags.splice(tagIndex, 1) };
-      } else {
-        return { ...state, tags: [...state.tags, action.payload.tag] };
-      }
-    }
+    switch (action.type) {
+        case "toggleTag": {
+            const tagIndex = state.tags.indexOf(action.payload.tag);
+            if (tagIndex !== -1) {
+                return { ...state, tags: state.tags.splice(tagIndex, 1) };
+            } else {
+                return { ...state, tags: [...state.tags, action.payload.tag] };
+            }
+        }
 
-    case "toggleFamilyFriendly": {
-      return { ...state, isFamilyFriendly: !state.isFamilyFriendly };
-    }
+        case "toggleFamilyFriendly": {
+            return { ...state, isFamilyFriendly: !state.isFamilyFriendly };
+        }
 
-    case "toggleVeganFriendly": {
-      state.isVeganFriendly = !state.isVeganFriendly;
-      return state;
-    }
+        case "toggleVeganFriendly": {
+            state.isVeganFriendly = !state.isVeganFriendly;
+            return state;
+        }
 
-    default:
-      throw Error();
-  }
+        default:
+            throw Error();
+    }
 };
 
 export interface RestaurantFiltersProps {
-  tags: string[];
-  onChange?: (value: RestaurantFiltersState) => unknown;
+    tags: string[];
+    onChange?: (value: RestaurantFiltersState) => unknown;
 }
 
 const FilterGroup = styled.div({
-  marginBottom: "1rem",
+    marginBottom: "1rem",
 });
 
 const FilterTitle = styled.h4({
-  marginBottom: "1rem",
-  fontSize: "1.1rem",
-  fontWeight: 500,
-  color: "#2c2c2c",
+    marginBottom: "1rem",
+    fontSize: "1.1rem",
+    fontWeight: 500,
+    color: "#2c2c2c",
 });
 
+const useStyles = makeStyles(theme => ({
+    root: {
+        display: 'flex',
+    },
+    //drawer: {
+    //    [theme.breakpoints.up('sm')]: {
+    //        width: drawerWidth,
+    //        flexShrink: 0,
+    //    },
+    //},
+    appBar: {
+        zIndex: theme.zIndex.drawer + 1,
+    },
+    menuButton: {
+        marginRight: theme.spacing(2),
+        [theme.breakpoints.up('sm')]: {
+            display: 'none',
+        },
+    },
+    toolbar: theme.mixins.toolbar,
+    //drawerPaper: {
+    //    width: drawerWidth
+    //},
+    content: {
+        flexGrow: 1,
+        padding: theme.spacing(3),
+    },
+    //closeMenuButton: {
+    //    marginRight: 'auto',
+    //    marginLeft: 0,
+    //},
+}));
+
+
+
 export function RestaurantFilters(props: RestaurantFiltersProps) {
-  const { tags = [], onChange } = props;
-  const [state, dispatch] = useReducer(restaurantFiltersReducer, {
-    tags: [],
-    isFamilyFriendly: false,
-    isVeganFriendly: false,
-  });
+    const { tags = [], onChange } = props;
+    const classes = useStyles();
+    const [state, dispatch] = useReducer(restaurantFiltersReducer, {
+        tags: [],
+        isFamilyFriendly: false,
+        isVeganFriendly: false,
+    });
 
-  useEffect(() => {
-    onChange?.(state);
-  }, [state, onChange]);
+    useEffect(() => {
+        onChange?.(state);
+    }, [state, onChange]);
 
-  return (
-    <div style={{ flex: "1 0 250px" }}>
-      <FilterGroup>
-        <FilterTitle>Tags</FilterTitle>
+    return (
+        <React.Fragment>
+            <CssBaseline />
+            <Container maxWidth="sm">
+                <FilterGroup>
+                    <FilterTitle>Tags</FilterTitle>
 
-        {tags.map((tag) => (
-          <ToggleFilter
-            label={tag}
-            isChecked={!!state.tags.includes(tag)}
-            onChange={() => dispatch({ type: "toggleTag", payload: { tag } })}
-          />
-        ))}
-      </FilterGroup>
+                    {tags.map((tag) => (
+                        <ToggleFilter
+                            label={tag}
+                            isChecked={!!state.tags.includes(tag)}
+                            onChange={() => dispatch({ type: "toggleTag", payload: { tag } })}
+                        />
+                    ))}
+                </FilterGroup>
 
-      <FilterGroup>
-        <FilterTitle>Other</FilterTitle>
+                <FilterGroup>
+                    <FilterTitle>Other</FilterTitle>
 
-        <ToggleFilter
-          label="Family friendly"
-          isChecked={state.isFamilyFriendly}
-          onChange={() => dispatch({ type: "toggleFamilyFriendly" })}
-        />
+                    <ToggleFilter
+                        label="Family friendly"
+                        isChecked={state.isFamilyFriendly}
+                        onChange={() => dispatch({ type: "toggleFamilyFriendly" })}
+                    />
 
-        <ToggleFilter
-          label="Vegan"
-          isChecked={state.isVeganFriendly}
-          onChange={() => dispatch({ type: "toggleVeganFriendly" })}
-        />
-      </FilterGroup>
-    </div>
-  );
+                    <ToggleFilter
+                        label="Vegan"
+                        isChecked={state.isVeganFriendly}
+                        onChange={() => dispatch({ type: "toggleVeganFriendly" })}
+                    />
+                </FilterGroup>
+            </Container>
+        </React.Fragment>
+    );
 }
--- RestaurantListings.UI/src/components/RestaurantList.tsx
+++ RestaurantListings.UI/src/components/RestaurantList.tsx
@@ -1,20 +1,37 @@
+import { makeStyles, Paper } from "@material-ui/core";
 import React from "react";
 
 import { Restaurant } from "../interfaces/restaurant";
 import { RestaurantItem } from "./RestaurantItem";
 
 export interface RestaurantListProps {
-  restaurants?: Restaurant[];
+    restaurants?: Restaurant[];
 }
 
-export function RestaurantList(props: RestaurantListProps) {
-  const { restaurants = [] } = props;
+const useStyles = makeStyles((theme) => ({
+    root: {
+        display: 'flex',
+        flexWrap: 'wrap',
+        '& > *': {
+            margin: theme.spacing(1),
+            width: theme.spacing(16),
+            height: theme.spacing(16),
+        },
+    },
+}));
 
-  return (
-    <div>
-      {restaurants.map((restaurant) => (
-        <RestaurantItem restaurant={restaurant} />
-      ))}
-    </div>
-  );
+export function RestaurantList(props: RestaurantListProps) {
+    const { restaurants = [] } = props;
+    const classes = useStyles();
+    return (
+        <div className={classes.root}>
+            {
+                restaurants.map((restaurant) => {
+                    <Paper elevation={5}>
+                        <RestaurantItem restaurant={restaurant} />
+                    <Paper>
+             })
+             }
+        </div>
+    );
 }
